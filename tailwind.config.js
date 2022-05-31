197d81137f3b65074c2787f256e729aeef1f263d
micolarighi <micolaarighi@gmail.com>
2022-06-01 00:00:49 +0700

    a few update and imporovement

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    colors: { 'primary': '#aa5ad6' },
+    colors: { 'primary': '#aa5ad6', 'gray-200': '#e5e7eb', 'gray-700': '#374151', 'white': '#fff', 'black': '#000' },
     extend: {},
   },
   plugins: [],
