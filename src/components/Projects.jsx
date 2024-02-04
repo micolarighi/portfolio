8af2a6ed9f6b102aa297e58c4d5d1221b39470ec
paradoxicalinsomniac <81902126+micolarighi@users.noreply.github.com>
2024-02-05 00:06:00 +0700

    added new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,7 @@ import Project3 from "../assets/img/Project3.png";
 import Project4 from "../assets/img/Project4.png";
 import Project5 from "../assets/img/Project5.png";
 import Project6 from "../assets/img/Project6.PNG";
+import Project7 from "../assets/img/Project7.PNG";
 
 import Helmet from "react-helmet";
 import "./Projects.css";
@@ -29,18 +30,18 @@ function Project(props) {
                 {props.Desc}
               </p>
 
-              {props.Unpublished ? 
-              <a></a>
-               :
-              <a
-                className="btn btn-dark"
-                href={props.Link}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <AiOutlineLink className="project-icon" />
-                &nbsp;&nbsp;Visit Website
-              </a>
+              {props.Unpublished ?
+                <a></a>
+                :
+                <a
+                  className="btn btn-dark"
+                  href={props.Link}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <AiOutlineLink className="project-icon" />
+                  &nbsp;&nbsp;Visit Website
+                </a>
               }
               <a
                 className="btn btn-secondary mt-2"
@@ -67,21 +68,23 @@ function Projects() {
         <title>Micola Arighi - Skills</title>
       </Helmet>
       <Container fluid className="project-wrapper mb-5 pb-5">
-          <div className="row animate__animated animate__slideInLeft">
+        <div className="row animate__animated animate__slideInLeft">
 
-            <Project Image={Project2} Title={"Online Clothing Store"} Desc={"Online Clothing Store for a local brand, Wekals. "} Link="https://wekals-web.vercel.app" Github={"https://github.com/micolarighi/wekals-web"}  />
+          <Project Image={Project7} Title={"Blog"} Desc={"A Blog filled with my thoughts. Specifically made to support my content creation on YouTube. "} Link="https://micolarighi-blog.vercel.app/" Github={"https://github.com/micolarighi/blog"} />
 
-            <Project Unpublished={true} Image={Project5} Title={"Online Course Platform"} Desc={"First Website that i build with a team as Front-End Developer"} Github={"https://github.com/relieyanhilman/edversity"}  />
+          <Project Image={Project2} Title={"Online Clothing Store"} Desc={"Online Clothing Store for a local brand, Wekals. "} Link="https://wekals-web.vercel.app" Github={"https://github.com/micolarighi/wekals-web"} />
 
-            <Project Image={Project6} Title={"VSCode Theme"} Desc={"Custom made Visual Studio Code theme. Check it out on your vscode extension, NGODING BRO"} Github={"https://github.com/micolarighi/my-vscode-theme"} Link={"https://marketplace.visualstudio.com/items?itemName=MicolaArighi.ngoding-bro"} />
+          <Project Unpublished={true} Image={Project5} Title={"Online Course Platform"} Desc={"First Website that i build with a team as Front-End Developer"} Github={"https://github.com/relieyanhilman/edversity"} />
 
-            <Project Image={Project1} Title={"React App - MiDo List"} Desc={"A Simple ToDo List App made with React. It uses Redux for state management and Framer Motion for animations. All the list will be stored to the local storage, so you can access them anytime."} Link={"https://midolist.netlify.app/"}  Github={"https://github.com/micolarighi/mido-app"} />
+          <Project Image={Project6} Title={"VSCode Theme"} Desc={"Custom made Visual Studio Code theme. Check it out on your vscode extension, NGODING BRO"} Github={"https://github.com/micolarighi/my-vscode-theme"} Link={"https://marketplace.visualstudio.com/items?itemName=MicolaArighi.ngoding-bro"} />
 
-            <Project Image={Project3}  Title={"LinkTree Clone"} Desc={"A Clone of linktree made with vanilla html and javascript."}  Link={"https://linkhub-micolarighi.netlify.app/"} 
-            Github={"https://github.com/micolarighi/micolarighi-linkhub"} 
-            />
+          <Project Image={Project1} Title={"React App - MiDo List"} Desc={"A Simple ToDo List App made with React. It uses Redux for state management and Framer Motion for animations. All the list will be stored to the local storage, so you can access them anytime."} Link={"https://midolist.netlify.app/"} Github={"https://github.com/micolarighi/mido-app"} />
 
-            <Project Image={Project4} Title={"Website Community Profile"} Desc={"Trusted to make A Community Profile for my Campus Community"} Unpublished={true} Github={"https://github.com/micolarighi/himatekkom"}  />
+          <Project Image={Project3} Title={"LinkTree Clone"} Desc={"A Clone of linktree made with vanilla html and javascript."} Link={"https://linkhub-micolarighi.netlify.app/"}
+            Github={"https://github.com/micolarighi/micolarighi-linkhub"}
+          />
+
+          <Project Image={Project4} Title={"Website Community Profile"} Desc={"Trusted to make A Community Profile for my Campus Community"} Unpublished={true} Github={"https://github.com/micolarighi/himatekkom"} />
 
 
         </div>
